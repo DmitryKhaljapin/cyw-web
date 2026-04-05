296dc23597bcce76a6736e5ab645c90ecc3012c3
DmitriX <dodge.241294@gmail.com>
2026-04-05 20:26:20 +0300

    add auth page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { Settings } from './pages/settings/settings';
+import { Auth } from './pages/auth/auth';
 
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'settings', component: Settings },
+  { path: 'auth', component: Auth },
 ];
---
+++ src/app/pages/auth/auth.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { Button } from '../../commin-ui/button/button';
+
+@Component({
+  selector: 'app-auth',
+  imports: [Button],
+  templateUrl: './auth.html',
+  styleUrl: './auth.css',
+})
+export class Auth {}
