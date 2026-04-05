732740e64a92fe9e3b766076a20a0e26c3e174be
DmitriX <dodge.241294@gmail.com>
2026-04-05 19:45:43 +0300

    add settings page

---
+++ src/app/pages/settings/settings.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { Button } from '../../commin-ui/button/button';
+
+@Component({
+  selector: 'app-settings',
+  imports: [Button],
+  templateUrl: './settings.html',
+  styleUrl: './settings.css',
+})
+export class Settings {}
